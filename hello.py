63e657c1a6eb0e27244dab8fb86d5c9b40c9ea69
omoteaki <omoteaki@gmail.com>
2023-11-03 22:16:23 +0900

    hello world

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+### Hello world
+
+print("hello, world")
+# print(hello, world)
+# 上2行では意味が違う
+
+hello = "hello,"
+world = "world"
+print(hello, world)
+print(hello, world, "haru", 1+1)
+print(1+1)
+# 変数が定義された後に変数を呼び出す
